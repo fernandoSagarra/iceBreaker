82361beb499bf5ebb88502a35cc885fb97902bd7
fernando <fernando.sagarra@yahoo.es>
2018-05-12 14:48:41 -0400

    Creating class to manage different databases in the future

---
+++ src/main/java/com/apptouch/ws/icebreaker/businesslogic/core/configuration/DataManager.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apptouch.ws.icebreaker.businesslogic.core.configuration;
+
+/**
+ *
+ * @author fernando
+ */
+public class DataManager {
+    
+    //private 
+    
+    public DataManager(){}
+    
+}
