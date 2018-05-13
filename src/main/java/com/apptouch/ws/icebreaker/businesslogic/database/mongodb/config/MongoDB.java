d9432ed0cce7c4caae392dabc8cb553076271999
fernando <fernando.sagarra@yahoo.es>
2018-05-13 17:31:58 -0400

    Refactoring

--- src/main/java/com/apptouch/ws/icebreaker/businesslogic/database/mongodb/config/MongoDB.java
+++ src/main/java/com/apptouch/ws/icebreaker/businesslogic/database/mongodb/config/MongoDB.java
@@ -1,14 +1,7 @@
 
 package com.apptouch.ws.icebreaker.businesslogic.database.mongodb.config;
 
-import com.apptouch.ws.icebreaker.businesslogic.database.mongodb.entities.BaseEntity;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
-import com.mongodb.ReadPreference;
-import com.mongodb.ServerAddress;
-import com.mongodb.WriteConcern;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
@@ -18,11 +11,11 @@ import org.mongodb.morphia.Morphia;
  */
 public class MongoDB {
     
-    private Morphia morphia;
-    private Datastore datastore;
-    private MongoClient mongoClient;
-    private String mappedPackage;
-    private String databaseName;
+    private final Morphia morphia;
+    private final Datastore datastore;
+    private final MongoClient mongoClient;
+    private final String mappedPackage;
+    private final String databaseName;
 
     
     public MongoDB(Morphia morphia, MongoClient mongoClient, String mappedPackage, String databaseName) throws ClassNotFoundException{
@@ -33,7 +26,7 @@ public class MongoDB {
         
         this.morphia.mapPackage(this.mappedPackage);
         
-        this.datastore = this.morphia.createDatastore(mongoClient, databaseName);
+        this.datastore = this.morphia.createDatastore(this.mongoClient, this.databaseName);
         
         this.datastore.ensureIndexes();
         this.datastore.ensureCaps();
